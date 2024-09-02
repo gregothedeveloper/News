91c35c89ff57939ca8a85a9732befe16c33eb0b1
gregothedeveloper <gregoforty2@gmail.com>
2024-09-02 04:46:46 +0300

    Pagination and filters

--- News/News/settings.py
+++ News/News/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'NewsApp',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
--- News/NewsApp/views.py
+++ News/NewsApp/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from . import models
 from .forms import CustomForm
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # Create your views here.
 def post_create(request,):
     if request.method == 'POST':
@@ -12,16 +11,28 @@ def post_create(request,):
             return redirect('list')
     else:
         form = CustomForm()
+        # title = 'create'
     return render(request, 'NewsApp/create.html', {'form': form})
 def post_list(request):
+    title = 'list'
     posts = models.Post.objects.all()
-    return render(request, 'NewsApp/list.html', {'posts': posts})
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+    try:
+        posts_paginated = paginator.page(page)
+    except PageNotAnInteger:
+        posts_paginated = paginator.get_page(1)
+    except EmptyPage:
+        posts_paginated = paginator.get_page(Paginator.num_pages)
+    return render(request, 'NewsApp/list.html',{'posts': posts_paginated, 'title': title})
 
 def post_detail(request, id):
+    title = 'detail'
     post = get_object_or_404(models.Post, id=id)
-    return render(request, 'NewsApp/detail.html', {'post': post})
+    return render(request, 'NewsApp/detail.html', {'post': post, 'title': title})
 
 def post_update(request, id):
+    
     post = get_object_or_404(models.Post, id=id)
     if request.method == 'POST':
         form = CustomForm(request.POST, instance=post)
@@ -30,11 +41,13 @@ def post_update(request, id):
             return redirect('detail', id=post.id)
     else:
         form = CustomForm(instance=post)
-        return render(request, 'NewsApp/update.html', {'form': form, 'post': post})
+        title = 'update'
+        return render(request, 'NewsApp/update.html', {'form': form, 'post': post, 'title':title})
     
 def post_delete(request, id):
     post = get_object_or_404(models.Post, id=id)
     if request.method == 'POST':
         post.delete()
         return redirect('list')
-    return render(request, 'NewsApp/delete.html', {'post': post})
+    title = 'delete'
+    return render(request, 'NewsApp/delete.html', {'post': post, 'title': title})
