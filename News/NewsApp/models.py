6a958e236a29594b446530ca1d05fee6e2b9ab45
gregothedeveloper <gregoforty2@gmail.com>
2024-09-06 12:44:30 +0300

    debugged

--- News/NewsApp/models.py
+++ News/NewsApp/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Author(models.Model):
-    name = models.CharField(max_length=60)
-    email = models.EmailField()
-    
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField()
-    author = models.models.ForeignKey(Author,on_delete=models.CASCADE)
+    author = models.CharField(max_length=60)
 
     def __str__(self):
         return f'{self.title} {self.content} {self.status} {self.author},{self.created_at}'
-    
